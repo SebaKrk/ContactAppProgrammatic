2b6452b150b58ef92764ec47a75699b708183958
SebaKrk <s.sciuba@gmail.com>
2020-12-22 18:07:01 +0100

    Initial Commit

---
+++ ContactAppProgrammatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactAppProgrammatic
+//
+//  Created by Sebastian Sciuba on 22/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
