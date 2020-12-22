2f41db696bcf992e54e97bf1c8d5c144365a3e94
SebaKrk <s.sciuba@gmail.com>
2020-12-22 22:44:53 +0100

    create first label and textField , add HelperFuncions - setUpTextField()

--- ContactAppProgrammatic/Controller/AddContactController.swift
+++ ContactAppProgrammatic/Controller/AddContactController.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class AddContactController : UIViewController {
     
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Add Contacts"
+        label.font = UIFont(name: "Helvetica-Bold", size: 30)
+        return label
+    }()
+    
+    let nameTF: UITextField = {
+        let textField = UITextField()
+        setUpTextField(textField, placeHolder: "name", keyboardType: UIKeyboardType.default)
+        return textField
+    }()
     
     
     override func viewDidLoad() {
@@ -35,4 +47,28 @@ class AddContactController : UIViewController {
         print("cancle")
         dismiss(animated: true, completion: nil)
     }
+}
+
+
+// MARK: - HelperFuncions
+
+func setUpTextField(_ textField: UITextField, placeHolder: String, keyboardType: UIKeyboardType) {
+    textField.translatesAutoresizingMaskIntoConstraints = false
+    textField.placeholder = placeHolder
+    textField.keyboardType = keyboardType
+    textField.returnKeyType = UIReturnKeyType.done
+    textField.autocorrectionType = UITextAutocorrectionType.no
+    textField.font = UIFont.systemFont(ofSize: 20)
+    textField.textColor = UIColor.label
+    textField.tintColor = UIColor.secondaryLabel
+    
+    textField.attributedPlaceholder = NSAttributedString(string: placeHolder, attributes:[NSAttributedString.Key.foregroundColor : UIColor.secondaryLabel])
+    
+    textField.borderStyle = UITextField.BorderStyle.roundedRect
+    textField.layer.borderColor = UIColor.secondaryLabel.cgColor
+    textField.layer.borderWidth = 0.5
+    textField.layer.cornerRadius = 5
+    textField.clearButtonMode = UITextField.ViewMode.whileEditing
+    textField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+    
 }
