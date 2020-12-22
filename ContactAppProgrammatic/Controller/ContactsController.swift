641748b213c27426f2a891b8e37ec135335d9e83
SebaKrk <s.sciuba@gmail.com>
2020-12-22 18:47:51 +0100

    setup tableView in ContactsController class

--- ContactAppProgrammatic/Controller/ContactsController.swift
+++ ContactAppProgrammatic/Controller/ContactsController.swift
@@ -9,10 +9,44 @@ import UIKit
 
 class ContactsController: UITableViewController {
     
+    private let reuseIdentifier = "cell"
+    
+    var contacts = [Contact]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        setUpBarbuttonItem()
+        registerTableView()
         
+    }
+    
+    //    MARK: - BarButtonItem
+    
+    func setUpBarbuttonItem() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(hanldeAddButton))
+    }
+    @objc func hanldeAddButton() {
+        print("add")
+    }
+    
+    //    MARK: - RegisterTableView
+    
+    func registerTableView() {
+        tableView.register(ContactsCell.self, forCellReuseIdentifier: reuseIdentifier)
+    }
+    
+    //    MARK: - TableView
+
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return contacts.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        
+        return cell
     }
 }
